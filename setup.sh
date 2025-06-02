e07cccf2fa7ff1a9766bbf79319789b4d2453ef3
l1f21bsse0537 <l1f21bsse0537@ucp.edu.pk>
2025-06-02 15:46:33 +0500

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+git init
+git branch -m develop
+git config --local user.name "l1f21bsse0537"
+git config --local user.email "l1f21bsse0537@ucp.edu.pk"
+git config --local core.editor notepad
+mkdir ./src ./styles ./bin ./assets ./node_modules
+touch ./src/index.html ./styles/index.css ./.gitignore
+git remote add origin https://github.com/OJ-Sagar-0537/s1week11s25.git
+
+echo "Hello world!" 
